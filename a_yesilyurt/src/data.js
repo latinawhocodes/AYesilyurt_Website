ce0dbdf305df78826545cbf51e368a8aeaeac0f3
Alyssa <alyssaroque01@gmail.com>
2023-12-23 15:03:55 -0800

    projects updated

---
+++ a_yesilyurt/src/data.js
@@ -0,0 +1,26 @@
+export const projects = [
+    {
+      title: "React Portfolio Project",
+      subtitle: "ReactJS, TailwindCSS",
+      description:
+        "My portfolio website built using ReactJS and Tailwind CSS.",
+      image: "./sbux_fall1.png",
+      // link: "https://reactbootcamp.com",
+    },
+    {
+      title: "Cat Factstory",
+      subtitle: "Python, Streamlit, JSON",
+      description:
+        "A cat facts website built using the Streamlit Library and Python.",
+        image: "./sbux_fall2.png",
+      // link: "https://reedbarger.com",
+    },
+    {
+      title: "MeowMoney",
+      subtitle: "ReactJS",
+      description:
+        "A budgeting app built with ReactJS and CSS.",
+        image: "./sbux_fall3.png",
+      // link: "https://jsbootcamp.com",
+    },
+  ];
