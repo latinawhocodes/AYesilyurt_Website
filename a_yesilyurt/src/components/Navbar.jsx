7be5fe3c5e49c26aaa288f12a7137229412c801b
Alyssa <alyssaroque01@gmail.com>
2023-12-23 16:15:34 -0800

    Update Navbar.jsx

--- a_yesilyurt/src/components/Navbar.jsx
+++ a_yesilyurt/src/components/Navbar.jsx
@@ -1,1 +1,36 @@
-export default function Navbar() {}
+// import { ArrowRightIcon } from "@heroicons/react/solid";
+import React from "react";
+
+function Navbar() {
+  return (
+    <header className="bg-gray-800 md:sticky top-0 z-10">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="title-font font-medium text-white mb-4 md:mb-0">
+          <a href="#about" className="ml-3 text-xl">
+            Alyssa Yesilyurt
+          </a>
+        </a>
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+          <a href="#projects" className="mr-5 hover:text-white">
+            Past Work
+          </a>
+          <a href="#skills" className="mr-5 hover:text-white">
+            Skills
+          </a>
+        </nav>
+        <a
+          href="#contact"
+          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+          Contact Me
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+            <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
+          </svg>
+
+
+        </a>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar; 
